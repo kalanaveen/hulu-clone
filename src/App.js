83a65a455f0342924636bd8589aee5456ee6a4d4
naveen <kalanaveen55@gmail.com>
2021-05-03 16:51:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, { useState } from "react";
+import Header from './Header';
+import Nav from './Nav';
+import Results from './Results';
+import requests from './requests';
+import Footer from './Footer'
+
+function App() {
+  const[selectedOption,setSelectedOption] = useState(requests.fetchTrending);
+  return (
+    <div className="app">
+      <Header/>
+      <Nav setSelectedOption={setSelectedOption}/>
+      <Results selectedOption={selectedOption}/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
